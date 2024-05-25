ed549853a5154f7d1ed001d3ff0c5db401d99564
antoniobarreiroimmune <antonio.barreiro.dw@immune.institute>
2024-05-25 11:32:28 +0200

    Ajuste pantalla

--- src/components/SubmitButton/SubmitButton.jsx
+++ src/components/SubmitButton/SubmitButton.jsx
@@ -1,15 +1,16 @@
-import { Button as ChakraButton } from "@chakra-ui/react"
+import { Flex, Button as ChakraButton } from "@chakra-ui/react"
 import React from "react"
 
 const SubmitButton = () => {
   return (
+    <Flex width="full" justifyContent="center" px={{ base: 4, md: 0 }}>
     <ChakraButton
       _hover={{
         backgroundColor: "brown",
       }}
       minH={"56px"}
       mx={{ base: 4, md: 0 }} 
-      width={{ base: "90%", md: "100%" }}
+      width={{ base: "70%", md: "100%" }}
       fontSize={"24px"}
       marginTop={"54px"}
       borderRadius={"20px"}
@@ -21,6 +22,7 @@ const SubmitButton = () => {
     >
       Submit
     </ChakraButton>
+    </Flex>
   )
 }
 
